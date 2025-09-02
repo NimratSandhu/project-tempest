f488753d31f0797be5c01d430e419ca56ba73274
Nimrat Sandhu <ns3sandh@uwaterloo.ca>
2025-09-01 21:56:43 -0400

    Button to add promos on Admin Page

--- app/static/js/admin.js
+++ app/static/js/admin.js
@@ -5,6 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const addOfferForm = document.getElementById("addOfferForm");
   const responseMsg = document.getElementById("responseMsg");
 
+  const openPromoFormBtn = document.getElementById("openPromoFormBtn");
+  const promoForm = document.getElementById("promoForm");
+  const cancelPromoBtn = document.getElementById("cancelPromoBtn");
+  const addPromoForm = document.getElementById("addPromoForm");
+
   // Show form
   openFormBtn.addEventListener("click", () => {
     offerForm.classList.remove("d-none");
@@ -45,6 +50,46 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error("Error:", error);
       responseMsg.style.color = "red";
       responseMsg.textContent = "Failed to add offer.";
+    });
+  });
+
+  openPromoFormBtn.addEventListener("click", () => {
+    promoForm.classList.remove("d-none");
+    openPromoFormBtn.classList.add("d-none");
+  });
+
+  cancelPromoBtn.addEventListener("click", () => {
+    promoForm.classList.add("d-none");
+    openPromoFormBtn.classList.remove("d-none");
+    addPromoForm.reset();
+    responseMsg.textContent = "";
+  });
+
+  addPromoForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const data = {
+      restaurant: document.getElementById("promo_restaurant").value,
+      description: document.getElementById("promo_description").value,
+      day_of_week: document.getElementById("promo_day_of_week").value,
+      active: document.getElementById("promo_active").checked
+    };
+
+    fetch("http://127.0.0.1:5000/promos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(result => {
+      responseMsg.style.color = "green";
+      responseMsg.textContent = "Promo added successfully!";
+      addPromoForm.reset();
+    })
+    .catch(error => {
+      console.error("Error:", error);
+      responseMsg.style.color = "red";
+      responseMsg.textContent = "Failed to add promo.";
     });
   });
 });
